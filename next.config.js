78ad46df14864a6a14e9b8a6166dfb9f1edf3ac7
DanielCG <danielcgilibert@gmail.com>
2023-03-09 19:13:56 +0100

    🔧 chore: image domain and remove console

--- next.config.js
+++ next.config.js
@@ -6,7 +6,11 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ['st3.depositphotos.com'],
+  },
   compiler: {
+    removeConsole: false,
     styledComponents: true,
   },
 }
